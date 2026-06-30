47ffb3c229ac3b84a4df4f6dc03cdfb8c92a9aa7
maimouna <ndao2001maimouma@gmail.com>
2026-06-30 20:48:44 +0000

    Ajout des validations de saisie

---
+++ imperative.php
@@ -0,0 +1,29 @@
+<?php
+$categories = [
+
+   0 =>      [
+            "code" => "01",
+            "nom" => "categorie1",
+            "produits" => [
+                  0 => [
+                    "nom" => "prodt1",
+                    "reference" => "reference1",
+                    "prix" => 8000,
+                    "quantite" => 3
+                  ],
+                  1 => [
+                    "nom" => "prodt2",
+                    "reference" => "reference2",
+                    "prix" => 200,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "02",
+            "nom" => "categorie2",
+            "produits" => []
+         ]
+];
+
+?>
