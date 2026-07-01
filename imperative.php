06d3a74f8d82a0f2e0aaaa5f4181e96e6b129f21
maimouna <ndao2001maimouma@gmail.com>
2026-07-01 02:57:45 +0000

    Ajout des validations de saisie

---
+++ imperative.php
@@ -0,0 +1,153 @@
+b<?php
+//1
+$categories = [
+
+   0 =>      [
+            "code" => "01",
+            "nom" => "categorie1",
+            "produits" => [
+                  0 => [
+                    "nom" => "prodt1",
+                    "reference" => "reference1",
+                    "prix" => 8000,
+                    "quantite" => 3
+                  ],
+                  1 => [
+                    "nom" => "prodt2",
+                    "reference" => "reference2",
+                    "prix" => 200,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "02",
+            "nom" => "categorie2",
+            "produits" => []
+         ]
+];
+//2
+    foreach($categories as $key ) {
+        if(empty($key["produits"])){
+            echo $key["nom"]."\n";
+        }
+    } 
+//3
+        $codValide = true;
+    do{
+        $code = readline("Saisie le code : ");
+        if (empty($code)){
+            echo "Le code est obligatiore.\n";
+            $codValide = false;
+        }
+       
+        foreach($categories as $key){
+            if ($key["code"]=== $code){
+                $codValide = false;
+                echo"le code exite deja. \n";
+                
+            }
+        }
+       
+    }while($codValide);
+
+     $nomValide = true;
+
+    do{
+        $nom = readline("Saisie le nom : ");
+        if (empty($nom)){
+            echo "Le code est obligatiore.\n";
+            $nomValide = false;
+        }
+       
+        foreach($categories as $key){
+            if ($key["nom"]=== $nom){
+                $nomValide = false;
+                echo"le code exite deja. \n";
+                
+            }
+        }
+       
+    }while($nomValide);
+
+//4
+    $categorie = [
+        "code" => $code,
+        "nom"  => $nom,
+        "produits" => []
+    ];
+
+    $categories[] = $categorie;
+
+    $categorieExit = false;
+    $code = readline("saisie le code: ");
+    foreach($categories as $key => $categorie){
+        if($categorie["code"]=== $code){
+            $categorieExit = true;
+        }
+    }
+    if ($categorieExit){
+        $produit = [
+            "nom" => readline("Saisie nom: "),
+            "reference" => readline("Saisie reference: "),
+            "prix" => (int)readline("Saisie prix: "),
+            "quantite" =>(int) readline("Saisie quantite: ")
+        ];
+        $categories[$key["produit"][] = $produit];
+    }
+    echo"la categorie n'exite pas \n"; 
+
+
+        $codValide = true;
+    do{
+        $code = readline("saisie le code: ");
+        if(empty($code)){
+            echo"code obligatoire \n";
+            $codValide = false;            
+        }
+        foreach($categories as $key){
+            if($key["code"]===$code){
+                $codValide = false;
+                echo"le code exite deja \n";
+            }
+        }
+
+    }while($codValide);
+
+
+     $nomValide = true;
+    do{
+        $nom = readline("saisie le nom: ");
+        if(empty($nom)){
+            echo"code obligatoire \n";
+            $nomValide = false;            
+        }
+        foreach($categories as $key){
+            if($key["nom"]===$nom){
+                $nomValide = false;
+                echo"le nom exite deja \n";
+            }
+        }
+
+    }while($nomValide);
+
+    $produit=[];
+do{
+    $produit = [
+        "nom" => readline("Saisie nom: "),
+        "reference" => readline("Saisie reference: "),
+        "prix" => (int)readline("Saisie prix: "),
+        "quantite" =>(int) readline("Saisie quantite: ")     
+    ];
+    $produits[] = produit;
+    $choix =  strtolower(redline("voulez continuer oui/non"));
+}while($choix === "oui");
+
+$categorie = [
+        "code" => $code,
+        "nom"  => $nom,
+        "produits" => $produits
+    ];
+
+$categories[] = $categorie;
+?>
