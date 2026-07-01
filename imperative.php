0950e46bc89d8e9ad75d2197872888769b46f904
maimouna <ndao2001maimouma@gmail.com>
2026-07-01 01:41:55 +0000

    Ajout des validations de saisie

--- imperative.php
+++ imperative.php
@@ -39,16 +39,15 @@ $categories = [
             echo "Le code est obligatiore.\n";
             $codvalide = false;
         }
-        $codExiste = true;
+       
         foreach($categories as $key){
             if ($key["code"]=== $code){
-                $codExiste = true;
-                break;
+                $codvalide = false;
+                echo"le code exite deja. \n";
+                
             }
         }
-        if($codExiste){
-            echo"le code exite deja. \n";
-        }
+       
     }while($codExiste);
 
 ?>
