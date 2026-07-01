9ccdfb384f2862da98853b63bd880205133a829e
maimouna <ndao2001maimouma@gmail.com>
2026-07-01 03:17:23 +0000

    Ajout des validations de saisie

--- procedu.php
+++ procedu.php
@@ -1,3 +1,43 @@
 <?php
 
+categories = [
+
+   0 =>      [
+            "code" => "01",
+            "nom" => "categorie1",
+            "produits" => [
+                  0 => [
+                    "nom" => "prodt1",
+                    "reference" => "reference1",
+                    "prix" => 8000,
+                    "quantite" => 3
+                  ],
+                  1 => [
+                    "nom" => "prodt2",
+                    "reference" => "reference2",
+                    "prix" => 200,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "02",
+            "nom" => "categorie2",
+            "produits" => []
+         ]
+];
+
+function afficheCategoriesSansProduit(array $categories): void{
+     foreach($categories as $key ) {
+        if(empty($key["produits"])){
+            echo $key["nom"]."\n";
+        }
+    } 
+}
+ afficheCategoriesSansProduit($categories);
+
+ function saisieChaine(string $messag):string{
+    return readline($messag);
+ }
+
 ?>
